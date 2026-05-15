e7e92c1cb575f658a27309bdea5034588a13188c
Ephraim Teodoro <ephraimteodoro@gmail.com>
2026-05-14 23:29:13 -1000

    fix path of css

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(expressLayouts);
 app.set("layout", "layout");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index", {
