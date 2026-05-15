e008cbf33f8529ac5c6e2bf7132fc44a8162e93e
Ephraim Teodoro <ephraimteodoro@gmail.com>
2026-05-14 23:09:33 -1000

    migrate learn html project to ejs format

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -9,17 +9,35 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "layout");
 
+app.get("/style.css", (req, res) => {
+  res.sendFile(`${__dirname}/style.css`);
+});
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index", {
-    title: "Home"
+    title: "Student Travel Guide | Home",
+    activePage: "home",
+    ctaText: "Plan a Trip",
+    ctaHref: "/destinations"
   });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About"
+app.get("/destinations", (req, res) => {
+  res.render("destinations", {
+    title: "Student Travel Guide | Destinations",
+    activePage: "destinations",
+    ctaText: "Photo Journal",
+    ctaHref: "/gallery"
+  });
+});
+
+app.get("/gallery", (req, res) => {
+  res.render("gallery", {
+    title: "Student Travel Guide | Gallery",
+    activePage: "gallery",
+    ctaText: "Start Exploring",
+    ctaHref: "/destinations"
   });
 });
 
